8a3109826248d6dacafb913a94ef93661f4409da
Nikhil Tellakula <nikhilt@umich.edu>
2020-11-07 09:47:25 -0800

    function scripts, live script pdf, raw data

---
+++ Week 6 - Applying Machine Learning/MATLAB/linearRegCostFunction.m
@@ -0,0 +1,39 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+	% LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+	%	regression with multiple variables
+	
+	%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+	%   	cost of using theta as the parameter for linear regression to fit the 
+	%   	data points in X and y. Returns the cost in J and the gradient in grad
+
+	% Initialize some useful values
+	m = length(y); % number of training examples
+
+	% You need to return the following variables correctly 
+	J = 0;
+	grad = zeros(size(theta));
+
+	% ====================== YOUR CODE HERE ======================
+	% Instructions: Compute the cost and gradient of regularized linear 
+	%               regression for a particular choice of theta.
+	%
+	%               You should set J to the cost and grad to the gradient.
+	%
+
+	% J: cost
+	h = X * theta;
+	term1 = (sum((h - y) .^2)) * (1 / (2 * m));
+	reg_term = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
+	J = term1 + reg_term;
+
+	% grad: Gradient
+	h = X * theta;
+	grad(1) = (X(:, 1)' * (h - y)) * (1 / m);
+	grad_reg = theta(2:end) * lambda / m;
+	grad(2:end) = (X(:, 2:end)' * (h - y)) * (1 / m) + grad_reg;
+
+	% =========================================================================
+
+	grad = grad(:);
+
+end
